89b6bbe46cd82cbf31dcb7b966767d65fa3ecd8e
Alexander Samorodov <blitzederich@gmail.com>
2020-10-27 22:45:04 +0300

    add devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const merge = require('webpack-merge').merge;
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const config = {
+const devConfig = {
+	mode: 'development',
 	entry: './src/webpack-template.js',
 	output: {
 		path: path.resolve('dist'),
@@ -31,19 +33,50 @@ const config = {
 	plugins: [new HtmlWebpackPlugin({
 		filename: 'index.html',
 		template: './src/index.html'
-	})]
-};
-
-const devConfig = {
-	mode: 'development'
+	})],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 8080,
+		open: true,
+	}
 };
 
 const prodConfig = {
-	mode: 'production'
+	mode: 'production',
+	entry: './src/webpack-template.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'js/[name].js?[contenthash]'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: ['babel-loader', 'ts-loader']
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css?[contenthash]'
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	]
 };
 
 module.exports = isProduction 
-	? merge(config, prodConfig)
-	: merge(config, devConfig);
-
-module.exports = config;	
+	? prodConfig
+	: devConfig;
